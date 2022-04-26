0f2d0d6c466f6ca7cec9a68a9bf4685a9b9112fc
Natalia <alt.antares@gmail.com>
2022-04-26 17:35:17 +0300

    created simple calc

---
+++ week13/simpleCalc/index.js
@@ -0,0 +1,27 @@
+function sum(){
+    let firstNumber = prompt("Введите число:");
+    let secondNumber = prompt("Введите число:");
+    let total = +firstNumber + +secondNumber;
+        alert(`Сумма равна ${total}`);
+    }
+
+function difference(){
+    let firstNumber = prompt("Введите число:");
+    let secondNumber = prompt("Введите число:");
+    let total = +firstNumber - +secondNumber;
+    alert(`Разность равна ${total}`);
+    }
+
+function work(){
+    let firstNumber = prompt("Введите число:");
+    let secondNumber = prompt("Введите число:");
+    let total = +firstNumber * +secondNumber;
+    alert(`Произведение равно ${total}`);
+    }
+
+function private(){
+    let firstNumber = prompt("Введите число:");
+    let secondNumber = prompt("Введите число:");
+    let total = +firstNumber / +secondNumber;
+    alert(`Частное равно ${total}`);
+    }
