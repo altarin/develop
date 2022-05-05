aee10838f489d0856a5e0c481b2bbe234fb87ba6
Natalia <alt.antares@gmail.com>
2022-05-06 00:35:21 +0300

    created message output

--- week14/helloName_input/index.js
+++ week14/helloName_input/index.js
@@ -1,10 +1,5 @@
 function getName() {
     let inputName = document.getElementById('enter_name').value;
+    document.getElementById('get_name').value = 'Привет, ' + inputName;
 
-    
-    // document.getElementById('get_name').innerHTML = inputName;
-    // document.innerHTML = ('Привет, ' + inputName);
-    // let textNode = document.createTextNode('А вот и я');
-
-
-  }
+}
