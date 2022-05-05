7aa155fdebce1b35e8abfe27034749a17d6cac1c
Natalia <alt.antares@gmail.com>
2022-05-05 23:37:58 +0300

    saved the first value

---
+++ week14/helloName_input/index.js
@@ -0,0 +1,10 @@
+function getName() {
+    let inputName = document.getElementById('enter_name').value;
+
+    
+    // document.getElementById('get_name').innerHTML = inputName;
+    // document.innerHTML = ('Привет, ' + inputName);
+    // let textNode = document.createTextNode('А вот и я');
+
+
+  }
