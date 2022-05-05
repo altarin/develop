e2aaf40a23f8710e032c9f53887a9f242f7c78b7
Natalia <alt.antares@gmail.com>
2022-05-06 00:22:11 +0300

    created gallery

---
+++ week14/gallery/index.js
@@ -0,0 +1,8 @@
+function changeImageForward(){
+    let flowerPic = document.getElementById('flowerPic');
+    flowerPic.src = "https://www.floristicplanet.by/wp-content/uploads/2018/03/glavnaya_narcissi.jpg";
+}
+function changeImageBack(){
+    let flowerPic = document.getElementById('flowerPic');
+    flowerPic.src = "https://www.floristicplanet.by/wp-content/uploads/2018/03/149881132959560bc1bf5889.53222802.jpg";
+}
