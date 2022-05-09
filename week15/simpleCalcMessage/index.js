51a9e727bae7f6f3a0138424513f69461d8e8d51
Natalia <alt.antares@gmail.com>
2022-05-09 21:31:15 +0300

    edited calc

---
+++ week15/simpleCalcMessage/index.js
@@ -0,0 +1,27 @@
+var a = document.getElementById('firstNumber').value;
+var b = document.getElementById('secondNumber').value;
+
+function getSum(a,b) {
+    
+    // let num1 = Number( document.getElementById('firstNumber').value );
+    // let num2 = Number( document.getElementById('secondNumber').value );
+    document.getElementById('sum') = a + b;
+    }
+
+function getDifference(){
+    let num1 = Number( document.getElementById('firstNumber').value );
+    let num2 = Number( document.getElementById('secondNumber').value );
+    document.getElementById('difference').value = num1 - num2;
+    }
+
+function getWork(){
+    let num1 = Number( document.getElementById('firstNumber').value );
+    let num2 = Number( document.getElementById('secondNumber').value );
+    document.getElementById('work').value = num1 * num2;
+    }
+
+function getPrivate(){
+    let num1 = Number( document.getElementById('firstNumber').value );
+    let num2 = Number( document.getElementById('secondNumber').value );
+    document.getElementById('private').value = num1 / num2;
+    }
