40fbc15bf762723cff203692167917a6fae1c20c
Natalia <alt.antares@gmail.com>
2022-05-17 01:01:37 +0300

    solved two tasks

---
+++ week16/taskWithArray/index.js
@@ -0,0 +1,19 @@
+// - 1. Дан массив `['js', 'css', 'html']`. Выведите на экран первый элемент
+
+let items =  ['js', 'css', 'html'];
+alert(items[0]);
+
+
+// 2. Отфильтруйте массив [0, 1, false, 2, undefined, '', 3, null] от нежелательных значений, таких как false, undefined, пустые строки, ноль, null с помощью метода filter.  Ожидаемый результат: [1, 2, 3].
+
+let values = [0, 1, false, 2, undefined, '', 3, null];
+const filteredValues = values.filter(el=>el !== false && el !== '' && el !== undefined && el !== 0 && el !== null);
+alert(filteredValues);
+
+
+// Дан массив [[1,2], [1,2,3], [1,2,3,4]]. Найдите индекс массива, длина которого > 3.
+
+let arr = [[1,2], [1,2,3], [1,2,3,4]];
+// let result = arr.map(function(elem) {
+// 	console.log(elem);
+// });
