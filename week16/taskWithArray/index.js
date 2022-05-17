7b0e587ff0a3d25caa37930521d44d46df0b2dd4
Natalia <alt.antares@gmail.com>
2022-05-17 22:40:40 +0300

    array index  found

--- week16/taskWithArray/index.js
+++ week16/taskWithArray/index.js
@@ -14,6 +14,8 @@ alert(filteredValues);
 // Дан массив [[1,2], [1,2,3], [1,2,3,4]]. Найдите индекс массива, длина которого > 3.
 
 let arr = [[1,2], [1,2,3], [1,2,3,4]];
-// let result = arr.map(function(elem) {
-// 	console.log(elem);
-// });
+for (let i = 0; i < arr.length; i++){
+    if (arr[i].length > 3) {
+        console.log(arr[i]);
+    }
+}
