2e99cae961e78bf371a21a55f6fd1b7b5cc94070
Natalia <alt.antares@gmail.com>
2022-06-21 23:05:50 +0300

    added function

--- week19/classCat/index.js
+++ week19/classCat/index.js
@@ -7,11 +7,17 @@ class Cat {
     }
 }
 
+function getInputs() {
+    return [document.querySelector('#name_pet').value, document.querySelector('#name').value, document.querySelector('#phone').value, document.querySelector('#email').value];
+}
+let button = document.querySelector('#elem');
+button.addEventListener('click', () => {
+    const values = getInputs();
+});
 let cat = new Cat('Феликс', 'Илья Иванович', '+7-921-123-4567', 'abc@gmail.com');
-elem.addEventListener("click", cat); 
+
 
 console.log(cat.nickname);
 console.log(cat.ownerName);
 console.log(cat.ownerPhone);
 console.log(cat.ownerEmail);
-
