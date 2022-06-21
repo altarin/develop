62504fa97ec8a363b8f343cb2f31edff8ec95112
Natalia <alt.antares@gmail.com>
2022-06-21 22:36:18 +0300

    added amount

--- week19/classCalc/index.js
+++ week19/classCalc/index.js
@@ -1,51 +1,53 @@
 class Calculator {
-    // constructor(number) {
-    //     this.number = number;
-    // }
 
     static amount(first, second){
         return first + second;
     }
-    // static subtraction(){
-    //     return this.firstNum - this.secondNum;
-    // }
-    // static multiplication(){
-    //     return this.firstNum * this.secondNum;
-    // }
-    // static quotient(){
-    //     return this.firstNum / this.secondNum;
-    // }
+    static subtraction(){
+        return first - second;
+    }
+    static multiplication(){
+        return first * second;
+    }
+    static quotient(){
+        return first / second;
+    }
 }
-let calculator = new Calculator('5', '7');
-console.log(Calculator.amount());
 
+//Глобальная переменная, которая хранит значение выбранной операции
+let currentOperation = "amount";
+// let currentOperationS = "subtraction";
 
+//Создаем функцию, которая будет возвращать массив со значениями инпута.
+function getNumbers() {
+    return [Number(document.querySelector('#num1').value), Number(document.querySelector('#num2').value)];
+}
+//Функция показа результата
+function showResult(result){
+    document.querySelector('#result').innerHTML = result;
+}
 
-// var operator; 
-// function calc() {
-//     var result;
-//     var num1 = Number(document.getElementById("num1").value);
-//     var num2 = Number(document.getElementById("num2").value);
-//     switch (operator) {
-//         case '+':
-//             result = num1 + num2;
-//             break;
-//         case '-':
-//             result = num1 - num2;
-//             break;
-//         case '*':
-//             result = num1 * num2;
-//             break;
-//         case '/':
-//             if (num2=='0') {
-//             result = 'На ноль делить нельзя!';
-//         } else {
-//             result = num1 / num2;
-//         }
-//         break;
-//     default:
-//         result = 'выберите операцию';
-//     }
-//     document.getElementById("result").innerHTML = result;
-// }
+const buttons = document.querySelectorAll('.operator'),
+    buttonResult = document.querySelector('#calcul');
+
+//Прикрепляем события на нажатие кнопок операций
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+//выясняем, какая операция назначена на кнопку через дата-атрибут и добавляем ее в глобальную переменную
+        currentOperation = e.target.dataset.operation;
+        // currentOperationS = e.target.dataset.operation; 
+    });
+});
+
+//событие на кнопке 
+buttonResult.addEventListener('click', () => {
+    const values = getNumbers(); //берем числа [2,2]
+//используем метод класса и переменную как ключ, деструктурируем массив со значениями
+    const result = Calculator[currentOperation](...values);
+    // result = Calculator[currentOperationS](...values);
+
+//показываем результат
+    showResult(result);
+});
+
 
