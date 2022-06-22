f4d0e8135501677a6e2edbaedcd92088f0819b38
Natalia <alt.antares@gmail.com>
2022-06-22 10:44:42 +0300

    edited class

--- week19/classCat/index.js
+++ week19/classCat/index.js
@@ -6,16 +6,24 @@ class Cat {
         this.ownerEmail = ownerEmail;
     }
 }
-
+//создаем функцию, которая возвращает массив со значениями инпутов
 function getInputs() {
     return [document.querySelector('#name_pet').value, document.querySelector('#name').value, document.querySelector('#phone').value, document.querySelector('#email').value];
 }
-let button = document.querySelector('#elem');
-button.addEventListener('click', () => {
-    const values = getInputs();
-});
-let cat = new Cat('Феликс', 'Илья Иванович', '+7-921-123-4567', 'abc@gmail.com');
+//Функция показа результата
+function showResult(result){
+    document.querySelector('#result').innerHTML = result;
+}
 
+let button = document.querySelector('#elem');
+//при клике на кнопку массив с данными сохраняется в переменной value
+button.addEventListener('click', () => {
+    const value = getInputs();
+});
+
+let cat = new Cat('Феликс', 'Илья Иванович', '+7-921-123-4567', 'abc@gmail.com');
+//показываем результат
+showResult(result);
 
 console.log(cat.nickname);
 console.log(cat.ownerName);
