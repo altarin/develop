f79ffe8d649923a8d8d88f01206d99bcafcc0505
Natalia <alt.antares@gmail.com>
2022-07-07 22:20:53 +0300

    created post request json

---
+++ week21/formRegistrPost/index.js
@@ -0,0 +1,51 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = getValues();
+    console.log(data);
+
+});
+
+function getValues() {
+    const inputs = document.querySelectorAll('input');
+    const data = {};
+
+    inputs.forEach(item => {
+        data[item.name] = item.value || '';
+    })
+return data;
+}
+
+
+let response = await fetch('/article/fetch/post/user', {
+    method: 'POST', 
+    headers: {
+'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(user)
+});
+
+let result = await response.json();
+alert(result.message);
+
+
+// validation
+
+// let errors = [];
+// function checkValidity(input) {
+//     let validity = input.validity;
+//     if (validity.valueMissing) {
+//         errors.push('Поле ' + input.placeholder + ' не заполнено');
+//     }
+// }
+
+// function checkAll(){
+// errors = [];
+// let inputs = document.querySelectorAll('input');
+//     for (let input of inputs){
+//         checkValidity(input);
+//     }
+//     document.getElementById('errorMessage').innerHTML = errors.join('. <br>');
+// }
